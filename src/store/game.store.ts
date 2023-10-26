3e790b2c50832f5efcd74073abfa5c67ce82cec2
Peter van der Noord <peterdunord@gmail.com>
2023-10-26 21:46:44 +0200

    Clear board state when setting new game

--- src/store/game.store.ts
+++ src/store/game.store.ts
@@ -19,41 +19,13 @@ type GameStore = {
 
 export const useGameStore = create<GameStore>((set) => ({
   gameDefinition: undefined,
-  // numberOfColumns: 10,
-  // numberOfRows: 10,
-  // rules: {
-  //   rows: [
-  //     [2, 4],
-  //     [9],
-  //     [2, 1, 3],
-  //     [1, 1, 2],
-  //     [1, 1, 2],
-  //     [2, 3],
-  //     [1, 4, 1],
-  //     [2, 3, 1],
-  //     [2, 1, 2],
-  //     [1, 1, 2],
-  //   ],
-  //   columns: [
-  //     [1, 1],
-  //     [4, 3],
-  //     [3, 1, 1],
-  //     [2, 3, 1, 1],
-  //     [2, 2],
-  //     [2, 4],
-  //     [2, 2, 1],
-  //     [3, 2],
-  //     [6, 2],
-  //     [4, 1, 1],
-  //   ],
-  // },
   boardState: {},
   setItemState: ({ itemState, boardPosition }) =>
     set((state) => ({
       boardState: { ...state.boardState, [getItemKey(boardPosition)]: itemState },
     })),
   setGameDefinition: (gameDefinition) => {
-    set(() => ({ gameDefinition }));
+    set(() => ({ gameDefinition, boardState: {} }));
   },
 }));
 
